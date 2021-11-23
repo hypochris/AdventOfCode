f3d5471b4c58052a414eca19253dc88ba0f5e118
Chris Reckinger <reckinger.chris@gmail.com>
2021-11-23 19:00:54 +0100

    day02

---
+++ day02/Program.cs
@@ -0,0 +1,18 @@
+﻿var data = File.ReadAllText("data.txt");
+
+var chars = data.ToCharArray();
+
+var index =1;
+var pos = 0;
+foreach (var c in chars)
+{
+    if (c == '(')
+        pos += 1;
+    else
+        pos -= 1;
+    
+    if(pos == -1)
+        break;
+    index++;
+}
+System.Console.WriteLine(index);
