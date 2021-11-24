10ca9c9c0f95148921f8b9b0da231cf335399bbd
Chris Reckinger <reckinger.chris@gmail.com>
2021-11-24 17:36:00 +0100

    minor changes

--- day03-1/Program.cs
+++ day03-1/Program.cs
@@ -1,19 +1,19 @@
 ﻿var data = File.ReadAllText("data.txt");
 
 var coord = (0, 0);
-var dict = new Dictionary<(int, int),int>(); 
+var dict = new Dictionary<(int, int),int>();
 dict.Add(coord, 1);
 
-foreach (var step in data.ToCharArray())
+foreach (var step in data)
 {
     if(step == '>')
-        coord.Item1 += 1;
+        coord.Item1++;
     else if(step == '<')
-        coord.Item1 -= 1;
+        coord.Item1--;
     else if(step == '^')
-        coord.Item2 += 1;
+        coord.Item2++;
     else if(step == 'v')
-        coord.Item2 -= 1;
+        coord.Item2--;
 
     if(!dict.Any(x => x.Key == coord))
         dict.Add(coord, 1);
@@ -21,4 +21,4 @@ foreach (var step in data.ToCharArray())
         dict[coord]++;
 }
 
-System.Console.WriteLine(dict.Count());
+System.Console.WriteLine(dict.Count);
--- day03-2/Program.cs
+++ day03-2/Program.cs
@@ -2,21 +2,21 @@
 
 var coord1 = (0, 0);
 var coord2 = (0, 0);
-var dict = new Dictionary<(int, int),int>(); 
+var dict = new Dictionary<(int, int),int>();
 dict.Add(coord1, 2);
 
 var count = 0;
-foreach (var step in data.ToCharArray())
+foreach (var step in data)
 {
     if(count % 2 == 0){
             if(step == '>')
-                coord1.Item1 += 1;
+                coord1.Item1++;
             else if(step == '<')
-                coord1.Item1 -= 1;
+                coord1.Item1--;
             else if(step == '^')
-                coord1.Item2 += 1;
+                coord1.Item2++;
             else if(step == 'v')
-                coord1.Item2 -= 1;
+                coord1.Item2--;
 
             if(!dict.Any(x => x.Key == coord1))
                 dict.Add(coord1, 1);
@@ -25,13 +25,13 @@ foreach (var step in data.ToCharArray())
     }
         else{
             if(step == '>')
-                coord2.Item1 += 1;
+                coord2.Item1++;
             else if(step == '<')
-                coord2.Item1 -= 1;
+                coord2.Item1--;
             else if(step == '^')
-                coord2.Item2 += 1;
+                coord2.Item2++;
             else if(step == 'v')
-                coord2.Item2 -= 1;
+                coord2.Item2--;
 
             if(!dict.Any(x => x.Key == coord2))
                 dict.Add(coord2, 1);
@@ -41,4 +41,4 @@ foreach (var step in data.ToCharArray())
     count++;
 }
 
-System.Console.WriteLine(dict.Count());
+System.Console.WriteLine(dict.Count);
