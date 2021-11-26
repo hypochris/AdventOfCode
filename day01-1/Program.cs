7e748c9402cc7bce72158c68142e7b0d08b459e8
Chris Reckinger <reckinger.chris@gmail.com>
2021-11-26 18:10:35 +0100

    move folder

--- day01-1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿var data = File.ReadAllText("data.txt");
-
-var chars = data.ToCharArray();
-
-var count = chars.Count(x => x == '(') - chars.Count(x => x == ')');
-
-System.Console.WriteLine(count);
