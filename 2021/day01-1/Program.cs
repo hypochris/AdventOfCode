ecf43a9cec9eeb7ab266f0372a1d4f00fea6fed3
Chris Reckinger <reckinger.chris@gmail.com>
2021-12-01 20:58:29 +0100

    2021 day 1

---
+++ 2021/day01-1/Program.cs
@@ -0,0 +1,11 @@
+﻿var data = File.ReadAllLines("data.txt");
+var dataInt = data.Select(x => int.Parse(x.Trim())).ToArray();
+
+var count = 0;
+for (int i = 0; i < dataInt.Length-1; i++)
+{
+    if(dataInt[i] < dataInt[i+1])
+        count++;
+}
+
+System.Console.WriteLine(count);
