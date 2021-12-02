c6a854620f0da159636a7adf9bfe5bf6c2baf922
Chris Reckinger <reckinger.chris@gmail.com>
2021-12-02 18:25:23 +0100

    2021 day2

---
+++ 2021/day02-2/Program.cs
@@ -0,0 +1,26 @@
+﻿var data = File.ReadAllLines("data.txt");
+
+var depth = 0;
+var horiz = 0;
+var aim = 0;
+
+foreach (var line in data)
+{
+    var direction = line.Split(" ")[0];
+    var num = int.Parse(line.Split(" ")[1]);
+
+    switch (direction)
+    {
+        case "forward":
+        horiz+=num;
+        depth+=aim*num;
+        break;
+        case "up":
+        aim-=num;
+        break;
+        case "down":
+        aim+=num;
+        break;
+    }   
+}
+System.Console.WriteLine(depth*horiz);
