e27b2392fee3bfb90e04821425bf0e5660bc18d8
Chris Reckinger <reckinger.chris@gmail.com>
2021-12-05 15:52:46 +0100

    2015 day08

--- 2015/day08-1/Program.cs
+++ 2015/day08-1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+var data = File.ReadAllLines("data.txt");
+
+var escaped = 0;
+var unescaped = 0;
+foreach (var line in data)
+{
+    escaped += line.Length;
+    unescaped += Regex.Unescape(line[1..^1]).Length;
+}
+
+System.Console.WriteLine(escaped - unescaped);
---
+++ 2015/day08-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+var data = File.ReadAllLines("data1.txt");
+
+var escaped = 0;
+var unescaped = 0;
+foreach (var line in data)
+{
+    unescaped += line.Length;
+    escaped += ToLiteral(line).Length;
+
+    System.Console.WriteLine($"{ToLiteral(line)} - {ToLiteral(line).Length}");
+}
+
+System.Console.WriteLine($"{escaped} - {unescaped}");
+System.Console.WriteLine(escaped - unescaped);
+
+static string ToLiteral(string valueTextForCompiler)
+{
+    return Microsoft.CodeAnalysis.CSharp.SymbolDisplay.FormatLiteral(valueTextForCompiler, false);
+}
