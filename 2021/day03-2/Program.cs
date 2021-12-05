4a277cfc94dcd1778eb1892400e6a330357de65b
Chris Reckinger <reckinger.chris@gmail.com>
2021-12-05 17:48:29 +0100

    2021 day03-2

---
+++ 2021/day03-2/Program.cs
@@ -0,0 +1,25 @@
+﻿var data = File.ReadAllLines("data.txt");
+
+var curData = data;
+
+foreach (var i in Enumerable.Range(0,data[0].Length))
+{
+    var challenge = curData.GroupBy(x => x[i]).OrderByDescending(x => x.Count()).ToDictionary(x => x.Key, x => x.Count());
+    var winner = challenge.ElementAt(0).Value == challenge.ElementAt(1).Value? '1' : challenge.ElementAt(0).Key;
+    curData = curData.Where(x => x[i] == winner).ToArray();
+
+    if (curData.Length == 1) break;
+}
+var oxy = Convert.ToInt16(curData[0], 2);
+
+curData = data;
+foreach (var i in Enumerable.Range(0,data[0].Length))
+{
+    var challenge = curData.GroupBy(x => x[i]).OrderBy(x => x.Count()).ToDictionary(x => x.Key, x => x.Count());
+    var winner = challenge.ElementAt(0).Value == challenge.ElementAt(1).Value? '0' : challenge.ElementAt(0).Key;
+    curData = curData.Where(x => x[i] == winner).ToArray();
+
+    if (curData.Length == 1) break;
+}
+var co = Convert.ToInt16(curData[0], 2);
+System.Console.WriteLine($"{oxy}, {co} = {oxy * co}");
